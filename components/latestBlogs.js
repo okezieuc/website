ded19102c747fa4b5b0d3d94da0afd975b53bca0
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-09-25 10:16:18 +0100

    create CTA and latest blog component

---
+++ components/latestBlogs.js
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import { dateInWords } from "../lib/dateInWords";
+import Container from "@components/container";
+
+function BlogPost({ post }) {
+  return (
+    <div class="w-full">
+      <div class="bg-gray-200 h-52 rounded-2xl relative"></div>
+      <div class="ml-4 mt-4 text-md sm:text-xl md:text-2xl">
+        <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+      </div>
+      <div class="ml-4 mt-3 font-medium md:font-bold text-gray-800 text-xs md:text-md">
+        {dateInWords(post.date)}
+      </div>
+    </div>
+  );
+}
+
+export default function LatestBlogs({ posts }) {
+  return (
+    <div class="py-12 md:py-32">
+      <Container>
+        <h2 class="font-heading text-4xl md:text-5xl mb-8 md:mb-16 font-bold">
+          From <span class="text-indigo-700">the Blog</span>
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <BlogPost post={posts[0]} />
+          <BlogPost post={posts[1]} />
+          <BlogPost post={posts[2]} />
+        </div>
+      </Container>
+    </div>
+  );
+}
