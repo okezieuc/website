71d3da0aea4667a61c67d6ab8d50f457e2d6391b
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-09-25 10:10:40 +0100

    redesign header and footer

--- components/header.js
+++ components/header.js
@@ -1,105 +1,34 @@
 import Link from "next/link";
 import { useState } from "react";
+import Container from "@components/container";
 
 export default function Header() {
   const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
 
   return (
-    <div class="w-full px-6">
-      <div class="mx-auto max-w-7xl">
-        <nav class="relative z-50 h-24 select-none">
-          <div class="container relative flex flex-wrap items-center justify-between h-24 mx-auto overflow-hidden font-medium border-b border-gray-200 md:overflow-visible lg:justify-center sm:px-4 md:px-2">
-            <div class="flex items-center justify-start w-1/4 h-full pr-4">
-              <Link href="/">
-                <a class="inline-block py-4 md:py-0">
-                  <span class="p-1 text-xl font-black leading-none text-gray-900">
-                    <span>studymono</span>
-                    <span class="text-indigo-600">.</span>
-                  </span>
-                </a>
-              </Link>
+    <div class="w-full">
+      <div class="bg-black w-full text-white py-2 text-sm">
+        <Container>
+          <Link href="/blog/past-questions-answers-and-explanations-published">
+            We're shuttting down public past question pages
+          </Link>
+        </Container>
+      </div>
+      <div class="text-sm md:text-lg bg-white border-b border-gray-300 py-2 md:py-4">
+        <Container>
+          <div class="flex flex-row items-center gap-4 md:gap-12">
+            <div class="text-lg md:text-2xl font-bold mr-1 md:mr-4">
+              <Link href="/">studymono.</Link>
             </div>
-            <div
-              class={`${
-                mobileMenuIsOpen ? "flex fixed" : "hidden"
-              } top-0 left-0 items-start w-full h-full p-4 text-sm bg-gray-900 bg-opacity-50 md:items-center md:w-3/4 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative md:flex`}
-            >
-              <div class="flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row">
-                <Link href="/">
-                  <a class="inline-flex items-center block w-auto h-16 px-6 text-xl font-black leading-none text-gray-900 md:hidden">
-                    studymono<span class="text-indigo-600">.</span>
-                  </a>
-                </Link>
-                <div class="flex flex-col items-start justify-center w-full space-x-6 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                  <Link href="/">
-                    <a class="inline-block w-full py-2 mx-0 ml-6 font-medium text-left text-indigo-600 md:ml-0 md:w-auto md:px-0 md:mx-2 lg:mx-3 md:text-center">
-                      Home
-                    </a>
-                  </Link>
-                  <Link href="/blog">
-                    <a class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">
-                      Blog
-                    </a>
-                  </Link>
-                  <Link href="/terms-and-conditions">
-                    <a class="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">
-                      Terms of Service
-                    </a>
-                  </Link>
-                </div>
-                <div class="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-1/3 md:flex-row md:py-0">
-                  <a
-                    href="#"
-                    class="hidden w-full px-6 py-2 mr-0 text-gray-700 md:px-0 lg:pl-2 md:mr-4 lg:mr-5 md:w-auto"
-                  >
-                    Sign In
-                  </a>
-                  <Link href="/blog">
-                    <a class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600">
-                      Coming soon: the platform
-                    </a>
-                  </Link>
-                </div>
-              </div>
+            <div class="hidden sm:block">
+              <Link href="/blog">Blog</Link>
             </div>
-            <div
-              onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
-              class="absolute right-0 flex flex-col items-center items-end justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100"
-            >
-              <svg
-                class={`w-6 h-6 text-gray-700 ${
-                  mobileMenuIsOpen ? "hidden" : "block"
-                }`}
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                x-cloak=""
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-              <svg
-                class={`w-6 h-6 text-gray-700 ${
-                  mobileMenuIsOpen ? "block" : "hidden"
-                }`}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                x-cloak=""
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+            <div class="flex-grow"></div>
+            <div class="bg-indigo-700 py-1 md:py-2 px-2 md:px-6 rounded-2xl	text-white hover:bg-indigo-600">
+              <a>Newsletter starting ending of September</a>
             </div>
           </div>
-        </nav>
+        </Container>
       </div>
     </div>
   );
