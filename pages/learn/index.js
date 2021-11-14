81abe0bc2c38bb37262ebb7a7c56739dd4411e1c
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-11-14 20:42:00 +0100

    add headings to courses homepage

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -1,5 +1,23 @@
 import Layout from "@components/app/layout";
 
 export default function CoursesPage() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className="max-w-xl">
+        <h1 className="text-4xl font-bold font-heading">Guided Courses</h1>
+        <div className="mt-4">
+          This is an opportunity to easily study difficult concepts. <br />
+          <br />
+          These quality materials will guide you to understand concepts from the
+          foundation to the apex of the topic.
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl font-heading">Your Courses</h2>
+      </div>
+      <div>
+        <h2 className="text-2xl font-heading">Explore</h2>
+      </div>
+    </Layout>
+  );
 }
