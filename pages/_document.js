097571e865654d876bdf0bd2bb22ee40e9194920
okezieuc <c.okezie.u@gmail.com>
2022-01-10 22:10:57 +0000

    add umami tracker

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,14 @@ class MyDocument extends Document {
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@studymono" />
           <meta name="twitter:creator" content="@OkezieChiedozie" />
+          <script
+            async
+            defer
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            data-host-url={process.env.NEXT_PUBLIC_UMAMI_DATA_HOST}
+            data-domains="www.studymono.com"
+            src="/umami.js"
+          />
         </Head>
         <body>
           <div class="font-body">
