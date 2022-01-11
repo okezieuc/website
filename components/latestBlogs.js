9f0d70367283974ca5d3ef7d7132d4561d7dcf31
okezieuc <c.okezie.u@gmail.com>
2022-01-11 20:01:12 +0000

    make landing page elements interactive

--- components/latestBlogs.js
+++ components/latestBlogs.js
@@ -5,7 +5,7 @@ import Container from "@components/container";
 
 function BlogPost({ post }) {
   return (
-    <div class="w-full">
+    <div class="w-full group">
       <Link href={`/blog/${post.slug}`}>
         <a>
           <div class="bg-gray-200 h-52 rounded-2xl relative">
@@ -19,7 +19,7 @@ function BlogPost({ post }) {
           </div>
         </a>
       </Link>
-      <div class="ml-4 mt-4 text-md sm:text-xl md:text-2xl">
+      <div class="ml-4 mt-4 text-md sm:text-xl md:text-2xl group-hover:underline">
         <Link href={`/blog/${post.slug}`}>{post.title}</Link>
       </div>
       <div class="ml-4 mt-3 font-medium md:font-bold text-gray-800 text-xs md:text-md">
