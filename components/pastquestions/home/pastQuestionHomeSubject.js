d2574030bb23a90b30f108eb917edf5f37224ed8
okezieuc <c.okezie.u@gmail.com>
2022-01-11 20:19:29 +0000

    make past question pages interactive

--- components/pastquestions/home/pastQuestionHomeSubject.js
+++ components/pastquestions/home/pastQuestionHomeSubject.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function PastQuestionHomeSubject({ subject, slug }) {
   return (
     <div className="my-6 sm:my-12">
-      <div className="flex gap-4 items-center">
+      <div className="flex group gap-4 items-center">
         <Link href={`/pastquestions/subject/${slug}/1`}>
           <a>
             <Image src={`/subjectcovers/${slug}.png`} width={96} height={96} />
           </a>
         </Link>
         <div>
-          <div className="text-3xl font-heading">
+          <div className="text-3xl font-heading group-hover:text-indigo-700 transition-colors">
             <Link href={`/pastquestions/subject/${slug}/1`}>{subject}</Link>
           </div>
           <div classname="mt-4">View {subject} Past Questions</div>
