171fc63ad01d8642cfc3ae52cc15de7495bc6c3e
okezieuc <c.okezie.u@gmail.com>
2022-02-24 16:02:36 +0000

    fixes on course homepage SEO tags

--- components/courses/page/coursePageSEO.js
+++ components/courses/page/coursePageSEO.js
@@ -3,26 +3,17 @@ import Head from "next/head";
 export default function CoursePageSEO({ title, longDescription, slug }) {
   return (
     <Head>
-      <title>{title}</title>
+      <title>{title} | Studymono Courses</title>
       <meta property="og:title" content={`${title} | Studymono Courses`} />
-      <meta
-        name="description"
-        content={`${longDescription} | Studymono Courses`}
-      />
-      <meta
-        property="og:description"
-        content={`${longDescription} | Studymono Courses`}
-      />
+      <meta name="description" content={longDescription} />
+      <meta property="og:description" content={longDescription} />
       <meta
         name="og:image"
         content="https://www.studymono.com/studymono-courses-cover.jpg"
       />
       <link rel="canonical" href={`https://www.studymono.com/learn/${slug}`} />
       <meta name="twitter:title" content={`${title} | Studymono Courses`} />
-      <meta
-        name="twitter:description"
-        content={`${longDescription} | Studymono Courses`}
-      />
+      <meta name="twitter:description" content={longDescription} />
       <meta
         name="twitter:image"
         content="https://www.studymono.com/studymono-courses-cover.jpg"
